d88ff6fc93cca402f375a17733c7bf9df7d41db6
Hala Fathy <hala.fathy@tedata.net>
2021-08-11 22:39:22 +0200

    starting docker and create docker image

--- IotDevicesManagement/src/test/java/com/vodafone/iot/business/DeviceManagementServiceTest.java
+++ IotDevicesManagement/src/test/java/com/vodafone/iot/business/DeviceManagementServiceTest.java
@@ -1,7 +1,7 @@
 package com.vodafone.iot.business;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
