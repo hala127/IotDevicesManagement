79b57752a56272d0043432a96ec567564aae2685
Hala Fathy <hala.fathy@tedata.net>
2021-08-14 14:24:04 +0200

    adding insertion queries to db script

--- DbScript.sql
+++ DbScript.sql
@@ -1,7 +1,19 @@
 CREATE DATABASE [IotDevices]
+GO
 
 USE [IotDevices]
 GO
+
+--create user to access the db
+CREATE LOGIN iot_devices 
+    WITH PASSWORD    = N'iot_dev',
+    CHECK_POLICY     = OFF,
+    CHECK_EXPIRATION = OFF;
+
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [iot_devices]
+
+ALTER SERVER ROLE [dbcreator] ADD MEMBER [iot_devices]
+--------------------------------------------------------------
 
 SET ANSI_NULLS ON
 GO
@@ -15,7 +27,7 @@ CREATE TABLE [dbo].[sim_status](
  CONSTRAINT [PK_sim_status] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
@@ -27,11 +39,11 @@ CREATE TABLE [dbo].[sim_card](
  CONSTRAINT [PK_sim_card] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
 UNIQUE NONCLUSTERED 
 (
 	[operator_code] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
@@ -49,7 +61,7 @@ CREATE TABLE [dbo].[device](
  CONSTRAINT [PK_device] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
@@ -68,4 +80,55 @@ INSERT INTO [dbo].[sim_status]
            ('Blocked'),
            ('Deactivated')
 GO
+
+INSERT INTO [dbo].[sim_card]
+           ([operator_code]
+           ,[country]
+           ,[status_id])
+     VALUES
+           ('01013599299', 'Egypt', 1),
+           ('01013599298', 'Albania', 2),
+           ('01013599297', 'Algeria', 3),
+           ('01013599296', 'Andorra', 4),
+           ('01013599295', 'Argentina', 4),
+           ('01013599294', 'Armenia', 2),
+           ('01013599293', 'Egypt', 2),
+           ('01013599292', 'England', 2),
+           ('01013599291', 'Australia', 3),
+           ('01013599290', 'Austria', 2),
+           ('01013599280', 'Barbados', 1),
+           ('01013599281', 'Egypt', 2),
+           ('01013599282', 'Italy', 1),
+           ('01013599283', 'Egypt', 2),
+           ('01013599284', 'Brazil', 2),
+           ('01013599285', 'Egypt', 2),
+           ('01013599286', 'Egypt', 2),
+           ('01013599287', 'Egypt', 3),
+           ('01013599288', 'Egypt', 2),
+           ('01013599289', 'Egypt', 1)
+GO
+
+INSERT INTO [dbo].[device]
+           ([sim_id]
+           ,[temperature])
+     VALUES
+           (1, 24),
+           (2, 25),
+           (3, 80),
+           (4, 24),
+           (5, 28),
+           (6, 90),
+           (7, 25),
+           (8, 29),
+           (9, 40),
+           (10, 50),
+           (11, 66),
+           (12, 44),
+           (13, 27),
+           (14, 28),
+           (15, 24),
+           (16, 25)
+GO
+
+
 
